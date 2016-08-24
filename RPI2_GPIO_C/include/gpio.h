4551c433b7789b05ff00e141209261b734aeaeb2
Kevin <kevgiso@hotmail.com>
2016-08-24 17:18:30 -0600

    Add pinMode function

--- RPI2_GPIO_C/include/gpio.h
+++ RPI2_GPIO_C/include/gpio.h
@@ -1,10 +1,14 @@
 #ifndef _GPIO_H
 #define _GPIO_H
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 int pinMode(int pin, int mode);
 int digitalWrite(int pin, int value);
 int digitalRead(int pin);
 int blink(int pin, int freq, int duration);
+int unexportPin(int pin);
+
 #endif
--- RPI2_GPIO_C/lib/gpio.c
+++ RPI2_GPIO_C/lib/gpio.c
@@ -1,7 +1,44 @@
 #include <gpio.h>
+#define RPI_GPIO_DIR "/sys/class/gpio/gpio"
+#define RPI_GPIO_EXPORT "/sys/class/gpio/export"
+#define MAXBUFFER 64	
+#define OUT 1
 
 int pinMode(int pin, int mode){
+//Variables declaration.
+FILE *exp; 
+FILE *direction;
+char modeValue[MAXBUFFER]; 
+char path[MAXBUFFER];
 
+//Export given pin.
+exp = fopen(RPI_GPIO_EXPORT, "w");
+if (exp == NULL)
+{
+    printf("Error opening file!\n");
+    exit(1);
+}
+
+fprintf(exp, "%s %d %s %s", "echo ", pin, " > ", RPI_GPIO_EXPORT);
+fclose(exp);
+
+//Assign port direction
+if(mode==0){
+	strcpy(modeValue, "out");
+}else{
+	strcpy(modeValue, "in");
+}
+
+//Write port direction to file
+direction = fopen(path, "w");
+if (direction == NULL)
+{
+    printf("Error opening file!\n");
+    exit(1);
+}
+
+fprintf(direction, "%s %s %s %s %d %s", "echo ", modeValue, " > ", RPI_GPIO_DIR, pin, "/direction");
+fclose(direction);
 
 return(0);
 }
@@ -20,6 +57,11 @@ return(0);
 
 int blink(int pin, int freq, int duration){
 
+
+return(0);
+}
+
+int unexportPin(int pin){
 
 return(0);
 }
